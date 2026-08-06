d787454f43b17edee0d6f738181cdb64ffd34e2e
Ankush25-anku <ankushshetty528@gmail.com>
2026-08-06 13:29:59 +0530

    Initial commit - Luhan Earth Movers website

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 "use client";
 
 import Hero from "@/sections/Hero/Hero";
+import About from "@/sections/About/About";
+import Services from "@/sections/Services/Services";
+import Projects from "@/sections/Projects/Projects";
 import Process from "@/sections/Process/Process";
 import WhyChooseUs from "@/sections/WhyChooseUs/WhyChooseUs";
 import Statistics from "@/sections/Statistics/Statistics";
+import Gallery from "@/sections/Gallery/Gallery";
 import Testimonials from "@/sections/Testimonials/Testimonials";
 import CTA from "@/sections/CTA/CTA";
 
@@ -11,9 +15,13 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
+      <Services />
+      <Projects />
       <Process />
       <WhyChooseUs />
       <Statistics />
+      <Gallery />
       <Testimonials />
       <CTA />
     </main>
